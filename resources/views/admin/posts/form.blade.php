fbe803037c9871a32db3839e1fc8e482799e160c
Renz Pabiton <renzpabiton@gmail.com>
2022-11-29 15:39:07 +0800

    Initial commit

---
+++ resources/views/admin/posts/form.blade.php
@@ -0,0 +1,20 @@
+<div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
+    <label for="title" class="control-label">{{ 'Title' }}</label>
+    <input class="form-control" name="title" type="text" id="title" value="{{ isset($post->title) ? $post->title : ''}}" >
+    {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
+</div>
+<div class="form-group {{ $errors->has('content') ? 'has-error' : ''}}">
+    <label for="content" class="control-label">{{ 'Content' }}</label>
+    <input class="form-control" name="content" type="text" id="content" value="{{ isset($post->content) ? $post->content : ''}}" >
+    {!! $errors->first('content', '<p class="help-block">:message</p>') !!}
+</div>
+<div class="form-group {{ $errors->has('date') ? 'has-error' : ''}}">
+    <label for="date" class="control-label">{{ 'Date' }}</label>
+    <input class="form-control" name="date" type="text" id="date" value="{{ isset($post->date) ? $post->date : ''}}" >
+    {!! $errors->first('date', '<p class="help-block">:message</p>') !!}
+</div>
+
+
+<div class="form-group">
+    <input class="btn btn-primary" type="submit" value="{{ $formMode === 'edit' ? 'Update' : 'Create' }}">
+</div>
